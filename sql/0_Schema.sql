e361f68f812941460af928eb2b0ef158aa95038e
hmizumoto <Hiroki.Mizumoto@sony.com>
2021-08-21 12:57:34 +0900

    create index for isu

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -15,7 +15,6 @@ CREATE TABLE `isu` (
    PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
-CREATE INDEX ix_isu_jia_isu_uuid on isu(jia_isu_uuid);
 CREATE INDEX ix_isu_character on isu(`character`);
 
 
